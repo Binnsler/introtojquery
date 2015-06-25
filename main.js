e0a11b00fe53ba1928c387c44527fbf8928094f4
Jamie Binns <jamie.m.binns@gmail.com>
2015-06-25 11:52:46 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Part 1 - The Good (<i>Establishing shot of broken down chuch, Western music plays</i>)
+
+// $('.first-button').click(function(){
+// 	$('body').append('<p>This is some new text.</p>');
+// });
+
+// $('.another-button').click(function(){
+// 	$('body').append('<h1>Some h1 text.</h1>');
+// });
+
+// $('.yet-another-button').click(function(){
+// 	$('body').append('<ul><li>List Item 1</li><li>List Item 2</li><li>List Item 3</li></ul>'
+// 		);
+// });
+
+// Part 2 - The Bad (<i>Establishing shot of saloon, with saloon piano music plays</i>)
+
